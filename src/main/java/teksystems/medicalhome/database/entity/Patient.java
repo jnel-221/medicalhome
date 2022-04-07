a09d97e3db6d5c6f30ed83350d7983f9e3a00da9
Jennifer Nelson <jennifer.nelson242@gmail.com>
2022-04-06 23:41:53 -0500

    created patient entity class

---
+++ src/main/java/teksystems/medicalhome/database/entity/Patient.java
@@ -0,0 +1,42 @@
+package teksystems.medicalhome.database.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "pass") // may need to change this based on DB structure.
+    private String password;
+
+    @EqualsAndHashCode.Exclude //exclude date from Equals and Hash methods
+    @ToString.Exclude //exclude date from toString method
+    @Column(name = "create_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate; 
+
+}
