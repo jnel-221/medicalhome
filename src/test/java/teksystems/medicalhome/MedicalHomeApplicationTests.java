25c393f0fca914eda92aef1108b65f4d27db028b
Jennifer Nelson <jennifer.nelson242@gmail.com>
2022-04-06 19:38:29 -0500

    Initial commit

---
+++ src/test/java/teksystems/medicalhome/MedicalHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package teksystems.medicalhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
