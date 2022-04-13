7013b1bc4269f8f72da6740810e805c632c27586
Jennifer Nelson <jennifer.nelson242@gmail.com>
2022-04-12 23:37:32 -0500

    created 500.jsp, added methods for handling 500 errors to errorcontroller

--- src/main/java/teksystems/medicalhome/controller/ErrorController.java
+++ src/main/java/teksystems/medicalhome/controller/ErrorController.java
@@ -1,12 +1,14 @@
 package teksystems.medicalhome.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
 import java.nio.file.AccessDeniedException;
 
@@ -15,7 +17,7 @@ import java.nio.file.AccessDeniedException;
 @ControllerAdvice
 public class ErrorController {
 
-    //need to make an error/404
+    //404 errors
     @ExceptionHandler(AccessDeniedException.class)
     @RequestMapping(value = "/error/404")
     public String error404(HttpServletRequest request) {
@@ -23,4 +25,54 @@ public class ErrorController {
         log.error("Requested URL not found : "+ request.getMethod()+ " " + originalUrl);
         return "error/404";
     }
+
+    //this method is for all 500 errors for any Exception that is thrown.
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleAllExceptions(HttpServletRequest request, Exception ex) {
+
+        log.error("Error page exception : " + getRequestURL(request), ex);
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("error/500");
+
+        String stackTrace = getHTMLStackTrace(ExceptionUtils.getStackFrames(ex));
+
+        model.addObject("requestUrl",  getRequestURL(request));
+        model.addObject("message", ex.getMessage());
+        model.addObject("stackTrace", stackTrace);
+
+        if ( ex.getCause() != null ) {
+            Throwable root = ExceptionUtils.getRootCause(ex);
+            model.addObject("rootcause", root);
+
+            String roottrace = getHTMLStackTrace(ExceptionUtils.getStackFrames(ex));
+            model.addObject("roottrace", roottrace);
+        }
+
+        return model;
+    }
+
+    private String getHTMLStackTrace(String[] stack) {
+        StringBuffer result = new StringBuffer();
+
+        for (String frame : stack) {
+            if (frame.contains("teksystems")) {
+                result.append(" &nbsp; &nbsp; &nbsp;" + frame.trim().substring(3) + "<br>\n");
+            } else if ( frame.contains("Caused by:")) {
+                result.append("Caused By: " + frame + "<br>");
+            }
+        }
+
+        return result.toString();
+    }
+
+    private String getRequestURL(HttpServletRequest request){
+        String result = request.getRequestURL().toString();
+        if(request.getQueryString() != null) {
+            result = result + "?" + request.getQueryString();
+        }
+        return result;
+    }
+
+
 }
